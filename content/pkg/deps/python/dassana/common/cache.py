3c9327e7149049b1c92ad5e4980fc3f0efa8f585
Jun Zhou <jun@dassana.io>
2021-09-23 17:44:51 -0700

    updated doc

--- content/pkg/deps/python/dassana/common/cache.py
+++ content/pkg/deps/python/dassana/common/cache.py
@@ -34,18 +34,28 @@ class _HashedTuple(tuple):
 _kwmark = (_HashedTuple,)
 
 
-def generate_hash(client_call, *args, **kwargs):
-    if issubclass(type(client_call), partial) and client_call.keywords.get('context') and issubclass(
-            type(client_call.keywords.get('context')), LambdaContext):
-        context = dict(filter(lambda x:
-                              x[0] in ['aws_access_key_id', 'aws_secret_access_key', 'aws_session_token'],
-                              client_call.keywords.get(
-                                  'context').client_context.env.items()))
-        kwargs = {
-            **kwargs,
-            **context
-        }
-    elif issubclass(type(kwargs.get('context')), LambdaContext):
+def dassana_generate_hash(client_call, *args, **kwargs) -> int:
+    """
+    Dassana hash function used for caching AWS clients. The hash key is on service, region, and LambdaContext (if
+    available). The most common case is to cache SDK clients with hashing on the service and region, and/or
+    AWS credentials.
+
+    For same-account client fetching, context is popped from **kwargs s.t hashing is done purely on the service and
+    region without interfering with client creation.
+
+    For cross-account client fetching, DassanaEngine injects AWS credentials exchanged through STS as custom env
+    in LambdaContext which is concatenated as a HashTuple with service and region to generate the hash. The creds
+    are unpacked as into **kwargs for hash generation (the sorting will ensure 1:1 hashing for identical objects
+    regardless of key order).
+
+    :param client_call: function
+    Not involved in the hashing scheme, it is just included in generate_hash as this function is wired
+    into the make_cached_call under config ure_ttl_cache.
+    :param args: arguments
+    :param kwargs: keyword arguments
+    :return:
+    """
+    if issubclass(type(kwargs.get('context')), LambdaContext):
         context = dict(filter(lambda x:
                               x[0] in ['aws_access_key_id', 'aws_secret_access_key', 'aws_session_token'],
                               kwargs.pop('context').client_context.env.items()))
@@ -57,10 +67,22 @@ def generate_hash(client_call, *args, **kwargs):
     return sum(sorted(kwargs.items()), _kwmark).__hash__()
 
 
-def configure_ttl_cache(maxsize=1024, ttl=60):
+def configure_ttl_cache(maxsize=1024, ttl=60, hash_op=dassana_generate_hash):
+    """
+    Decorator that initializes a TTL Cache which is utilized in any subsequent function calls with the hashing key
+    defined generate_hash. The following implementation is built as part of higher order functions to enable
+    flexibility: the biggest benefit is caching can be deployed on any function calls throughout Dassana Actions whereby
+    the intention / control flow can be
+
+    :param maxsize: maximium size of the TTL cache
+    :param ttl: time to live (in seconds) of items in cache
+    :param hash_op: function with hashing scheme applying to the same args that make_cached_call consumes
+    :return: a function with another function as its first parameter which calls on keyword arguments
+             f_1(f_2(name1=value1, ..., nameN=valueN), keywords: name1=value1, name2=value2, nameN=valueN) -> f_2
+    """
     cache = TTLCache(maxsize=maxsize, ttl=ttl)
 
-    @cached(cache, key=generate_hash)
+    @cached(cache, key=hash_op)
     def make_cached_call(client_call, *args, **kwargs) -> client:
         return client_call(**kwargs)
 
